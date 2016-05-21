694dadf7ed4389a7f8b712b7d924de338138efdc
Kyon <cryzar95@googlemail.com>
2016-05-21 14:40:52 +0200

    new prepost update

---
+++ RARETaliyah/RARETaliyah/Taliyah/Passive.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using LeagueSharp;
+using LeagueSharp.SDK;
+using SharpDX;
+
+namespace RARETaliyah.Taliyah
+{
+    internal class Passive
+    {
+
+        internal bool IsActive => GameObjects.Player.HasBuff(""); //TODO: Add the proper buffname.
+        internal int Range { get; private set; }
+
+        /// <summary>
+        /// Constuctor should be called at first. 
+        /// </summary>
+        /// <param name="range">a proper integer that specifes the passive range</param>
+        public Passive(int radius)
+        {
+            this.Range = radius * 2;
+        }
+
+        /// <summary>
+        /// class for storing Taliyah's Passivs.
+        /// </summary>
+        internal class Passiv
+        {
+            int Range;
+            Vector2 Position;
+
+            public Passiv(int range, Vector2 position)
+            {
+                this.Range = range;
+                this.Position = position;
+            }
+        }
+
+        /// <summary>
+        /// GetAllCurrentPassives in Range of the position you gave.
+        /// </summary>
+        /// <param name="range">an integer value that provides a proper range</param>
+        /// <param name="position"> </param>
+        /// <returns></returns>
+        internal List<Passiv> GetCurrentPassives(Vector2 position, int range)
+        {
+            //Method to catch up current passive in range on this position.
+            return new List<Passiv>();
+        }
+
+    }
+}
---
+++ RARETaliyah/RARETaliyah/Taliyah/SpellR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.SDK;
+
+namespace RARETaliyah.Taliyah
+{
+    internal class SpellR
+    {
+
+        public Spell SpellDetails { get; private set; }
+
+        public SpellR(Spell r)
+        {
+            this.SpellDetails = r;
+        }
+
+        public double CustomEDamage(Obj_AI_Base t)
+        {
+
+
+            return double.MinValue;
+        }
+
+    }
+}
